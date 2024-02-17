1ab9683bfd038b53b54827d54c056fdc7c4c93fa
Chanjin Lee <chanjin23@naver.com>
2024-02-17 16:01:50 +0900

    field-column mapping commit

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.Date;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -18,24 +19,16 @@ public class JpaMain {
         tx.begin();
 
         try{
-            //비영속 상태
             Member member = new Member();
-            member.setId(101L);
-            member.setName("HelloJPA");
-
-            //영속 상태  DB에 저장이 되는것은 아님
-            System.out.println("==BEFORE==");
-            em.persist(member); //1차 캐시 저장
-//            em.detach(member);  //영속성 컨텍스트 분리
-            System.out.println("==AFTER==");
-
-            Member findMember = em.find(Member.class, 101L);    //1차캐시에 있는 곳을 찾는다.
-            System.out.println("findMember.getId() = " + findMember.getId());
-            System.out.println("findMember.getName() = " + findMember.getName());
+            member.setId(4L);
+            member.setAge(10);
+            member.setUsername("hello");
+            member.setRoleType(RoleType.USER);
+            
+            em.persist(member);
 
             tx.commit();
             //commit이 되면 DB로 넘어가게됨.
-
         }catch(Exception e){
             tx.rollback();
         }finally{
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,13 +1,36 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity //JPA를 사용하는 애 구나 하고 인식을하게됨
+//@Table(name = "MBR")    //이런식으로하면 table과 클래스가 매핑될때 MBR가 매핑된다.
 public class Member {
     @Id //PK가 뭔지 알려줘야함
     private Long id;
-    private String name;
+
+    @Column(name = "name",nullable = false)
+    //Member 클래스에는 username인데 데이터베이스에는 name이라는 필드명을 쓰고싶으면 다음과 같이 name을 변경해준다.
+    private String username;
+    private Integer age;    //다른타입을 쓸수도있음 DB에서도 숫자타입이 적용됨.
+
+    @Enumerated(EnumType.STRING)
+    //자바에는 enum타입이 있지만 db에는 enum타입이 없다. 그래서 다음과 같이 enumtype을 붙인다.
+    private RoleType roleType;
+
+    //타입 정보가 3개존재한다. 시간, 날짜, 날짜시간
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
+
+    public Member(){
+
+    }
 
     public Long getId() {
         return id;
@@ -17,11 +40,51 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
